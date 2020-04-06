c0416f76e9dd17e4d986f2473da5b70c963d9f5c
procopio420 <lucas.p@gec.inatel.br>
2020-04-06 19:08:03 -0300

    projeto pixel-art

--- projects/meme-generator/script.js
+++ projects/meme-generator/script.js
@@ -7,8 +7,7 @@ let imgInput = document.getElementById('meme-insert');
 let img = document.getElementById('meme-image');
 
 imgInput.addEventListener('input', () => {
-    console.log(imgInput.files[0].name);
-    img.src = URL.createObjectURL(event.target.files[0]);
+  img.src = URL.createObjectURL(event.target.files[0]);
 });
 
 let btn1 = document.getElementById('button1');
@@ -16,14 +15,14 @@ let btn2 = document.getElementById('button2');
 let btn3 = document.getElementById('button3');
 let div = document.getElementById('meme-image-container');
 
-btn1.addEventListener('click', ()=>{
-    div.style.border='3px dashed red';
+btn1.addEventListener('click', () => {
+  div.style.border = '3px dashed red';
 });
-btn2.addEventListener('click', ()=>{
-    div.style.border='5px double blue';
+btn2.addEventListener('click', () => {
+  div.style.border = '5px double blue';
 });
-btn3.addEventListener('click', ()=>{
-    div.style.border='6px groove green';
+btn3.addEventListener('click', () => {
+  div.style.border = '6px groove green';
 });
 
 let btn4 = document.getElementById('meme-1');
@@ -31,15 +30,15 @@ let btn5 = document.getElementById('meme-2');
 let btn6 = document.getElementById('meme-3');
 let btn7 = document.getElementById('meme-4');
 
-btn4.addEventListener('click', ()=>{
-    img.src='./imgs/meme1.jpeg';
+btn4.addEventListener('click', () => {
+  img.src = './imgs/meme1.jpeg';
 });
-btn5.addEventListener('click', ()=>{
-    img.src='./imgs/meme2.jpeg';
+btn5.addEventListener('click', () => {
+  img.src = './imgs/meme2.jpeg';
 });
-btn6.addEventListener('click', ()=>{
-    img.src='./imgs/meme3.jpeg';
+btn6.addEventListener('click', () => {
+  img.src = './imgs/meme3.jpeg';
 });
-btn7.addEventListener('click', ()=>{
-    img.src='./imgs/meme4.jpeg';
+btn7.addEventListener('click', () => {
+  img.src = './imgs/meme4.jpeg';
 });
