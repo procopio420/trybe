300efe086fdfba804ac55296b73cd61b1ed82be5
procopio420 <lucas-procopio@outlook.com>
2020-05-29 19:37:51 -0300

    exercicio pronto

---
+++ exercises/12_2/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import pokemons from './data';
+
+import Pokedex from './components/Pokedex';
+import Pokemon from './components/Pokemon'
+
+import './style.css'
+
+function App() {
+  return (
+    <Pokedex>
+      {pokemons.map(p => (
+        <Pokemon
+          key={p.id}
+          name={p.name}
+          type={p.type}
+          weight={p.averageWeight.value}
+          unit={p.averageWeight.measurementUnit}
+          img={p.image}
+          info={p.moreInfo}
+        />
+      ))}
+    </Pokedex>
+  );
+}
+
+export default App;
